ed82e1b91ea6a99d68d5ab6f83d1fbabe7c0c328
ShreyaPrabhu03 <nnm22ec161@nmamit.in>
2025-07-23 11:49:20 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<ctype.h>
+#include<string.h>
 
 int main()
 {
-    char sn[100];
-    printf("enter your name");
-    scanf("%[^\n]s",&sn);//for space aslo like abs bce dec
-    printf("address is = %s",sn);
-    return 0;
+   char s[100];
+int   sum=0;
+   int n,i,j;
+   scanf("%s",&s);
+   for(i=0;s[i]!='0';i++)
+   {
+
+       if(isdigit(s[i]))
+       {
+
+        n=s[i]-'0';
+        //0 bcs ascii will be stored in order to convert to normal number we use
+        for(j=i+1;s[j]!='\0';j++)
+        {
+
+
+        if(isdigit(s[j]))
+            n=n*10+s[j]-'0';
+
+else{
+    i=j;
+    break;
+}
+      }
+sum=sum+n;
+
+   }}
+   printf("%d",sum);
+   return 0;
 }
